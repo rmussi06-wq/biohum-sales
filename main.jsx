e509057a37dca24d7582d5ef37f80ce6c66c649d
rmussi06-wq <rmussi06@gmail.com>
2026-02-25 01:54:57 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './styles/app.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
+// Hide the static splash after first paint
+requestAnimationFrame(() => {
+  if (window.__hideBootSplash) window.__hideBootSplash()
+})
